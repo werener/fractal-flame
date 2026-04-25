e06356eb70fdc1a17882450aeb5b400af63a6bfb
werener <lenyazhuk@yandex.ru>
2026-04-25 21:36:24 +0300

    feat: add flag validation TODO: add outputPath and configPath validation

--- internal/infrastructure/cli/flag_validation.go
+++ internal/infrastructure/cli/flag_validation.go
@@ -1,11 +1,72 @@
 package cli
 
-func validateDimension(_ int) error
-func validateIterationCount(_ int) error
-func validateOutput(_ string) error
-func validateThreads(_ int) error
-func validateAffineParams(_ []float64) error
-func validateFunctions(_ []string) error
-func validateConfig(_ string) error
-func validateGamma(_ float64) error
-func validateSymmetryLevel(_ int) error
+import (
+	"errors"
+	"fmt"
+)
+
+var (
+	errWrongDimension     error = errors.New("dimension has to be higher than zero")
+	errIterationCount     error = errors.New("iteration count has to be higher than zero")
+	errThreadsCount       error = errors.New("thread count has to be higher than zero")
+	errAffineParams       error = errors.New("wrong amount of affine params")
+	errWrongGamma         error = errors.New("gamma cannot be zero")
+	errWrongSymmetryLevel error = errors.New("symmetry level has to be higher than zero")
+)
+
+func validateDimension(d int) error {
+	if d < 1 {
+		return errWrongDimension
+	}
+	return nil
+}
+
+func validateIterationCount(i int) error {
+	if i < 1 {
+		return errIterationCount
+	}
+	return nil
+}
+
+// TODO
+func validateOutput(_ string) error {
+	return nil
+}
+
+func validateThreads(t int) error {
+	if t < 1 {
+		return errThreadsCount
+	}
+	return nil
+}
+
+func validateAffineParams(ap []float64) error {
+	if len(ap) != 6 {
+		return fmt.Errorf("%w: %d", errAffineParams, len(ap))
+	}
+	return nil
+}
+
+func validateFunctions(funcStrs []string) error {
+	_, err := parseFunctions(funcStrs)
+	return err
+}
+
+// TODO
+func validateConfig(_ string) error {
+	return nil
+}
+
+func validateGamma(g float64) error {
+	if g == 0 {
+		return errWrongGamma
+	}
+	return nil
+}
+
+func validateSymmetryLevel(sl int) error {
+	if sl < 1 {
+		return errWrongSymmetryLevel
+	}
+	return nil
+}
