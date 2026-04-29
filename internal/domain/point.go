461bf082a1db3a71720d4ae50d2f4641e042ddf0
werener <lenyazhuk@yandex.ru>
2026-04-29 14:08:46 +0300

    feat: add affine transformation method for the point class

--- internal/domain/point.go
+++ internal/domain/point.go
@@ -9,3 +9,10 @@ type Point struct {
 func NewPoint(x, y float64) Point {
 	return Point{X: x, Y: y}
 }
+
+// affineTransform applies and affine transformation
+// to the point, mutating it.
+func (p *Point) affineTransform(ap AffineParams) {
+	p.X = p.X*ap.A + p.Y*ap.B + ap.C
+	p.Y = p.X*ap.D + p.Y*ap.E + ap.F
+}
