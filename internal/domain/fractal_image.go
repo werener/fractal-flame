f7152acac7f57cab4ec9f616cc6964ab190b79f8
werener <lenyazhuk@yandex.ru>
2026-04-30 12:54:55 +0300

    feat:  create a method to generate the fractal image

--- internal/domain/fractal_image.go
+++ internal/domain/fractal_image.go
@@ -1,5 +1,7 @@
 package domain
 
+import "github.com/werener/fractal-flame/pkg/random"
+
 const Shift = 20 // Amount of iterations to determine the startng point
 
 // FractalImage represents a fractal image.
@@ -20,14 +22,39 @@ func NewFractalImage(width, height int) *FractalImage {
 
 // GetPixel returns the pixel at the specified coordinates (x, y)
 // if they are within the bounds of the image.
-func (f *FractalImage) GetPixel(x, y int) (*Pixel, bool) {
-	if !f.contains(x, y) {
+func (fi *FractalImage) GetPixel(x, y int) (*Pixel, bool) {
+	if !fi.contains(x, y) {
 		return nil, false
 	}
 
-	return &f.Pixels[y*f.Width+x], true
+	return &fi.Pixels[y*fi.Width+x], true
 }
 
-func (f *FractalImage) contains(x, y int) bool {
-	return x >= 0 && y >= 0 && x < f.Width && y < f.Height
+func (fi *FractalImage) contains(x, y int) bool {
+	return x >= 0 && y >= 0 && x < fi.Width && y < fi.Height
+}
+
+// Generate creates the fractal image within the specified rectangle.
+// TODO: make it non-reliant on configuraion.
+func (fi *FractalImage) Generate(
+	rect Rectangle,
+	cfg *Configuration,
+	color Color,
+	rnd random.Random,
+) {
+	point := rect.RandomPoint(rnd)
+	for i := range Shift + cfg.IterationCount {
+		point.affineTransform(cfg.AffineParams)
+
+		transformation := GetRandomTransformation(rnd, cfg.Functions)
+		point = transformation(point)
+
+		if i >= 0 {
+			if rect.Contains(point) {
+				if pixel, ok := point.project(rect, fi); ok {
+					pixel.ColorPixel(color)
+				}
+			}
+		}
+	}
 }
--- internal/domain/point.go
+++ internal/domain/point.go
@@ -16,3 +16,11 @@ func (p *Point) affineTransform(ap AffineParams) {
 	p.X = p.X*ap.A + p.Y*ap.B + ap.C
 	p.Y = p.X*ap.D + p.Y*ap.E + ap.F
 }
+
+// project maps the point to a pixel in the fractal image.
+func (p Point) project(rect Rectangle, fi *FractalImage) (*Pixel, bool) {
+	x := int((p.X - rect.MinPoint.X) / rect.Width * float64(fi.Width))
+	y := int((p.Y - rect.MinPoint.Y) / rect.Height * float64(fi.Height))
+
+	return fi.GetPixel(x, y)
+}
